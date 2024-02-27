3b3b7334e20a410ff7c40bc63f1594a2666333eb
Emmanuek5 <emmanuelandrew625@yahoo.com>
2024-02-27 21:48:30 +0100

    Update server

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const app = express(); // Your express app
 
 const webhook = new Topgg.Webhook(config.topgg.webhookauth); // add your Top.gg webhook authorization (not bot token)
 
+app.get("/", (req, res) => {
+  res.send("ClovenBots API");
+});
+
 module.exports = {
   webhook: webhook,
   app: app,
